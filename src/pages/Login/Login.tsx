bf7553895688d04b0087fd7fdd6b2788f1a28be9
luizgustavojunqueira <luizgustavossj@gmail.com>
2024-09-02 14:10:28 -0400

    feat: login page style

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -2,6 +2,8 @@ import { useState } from "react";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
 
+import "./login.css";
+
 function Login() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
